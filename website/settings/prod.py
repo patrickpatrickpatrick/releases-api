8900fafaedc5a2c0a437c25419e7a26872951055
root <root@patrickpages.com>
2017-07-31 22:34:26 +0100

    setitings and wsgi changed

--- website/settings/prod.py
+++ website/settings/prod.py
@@ -1,1 +1,19 @@
 from website.settings.base import *
+
+ALLOWED_HOSTS = ['patrickpages.com', '178.79.131.15']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'label_api',
+        'USER': 'label_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = '/label_api/website/settings/static'
+
